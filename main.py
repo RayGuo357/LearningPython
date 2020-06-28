d5d62f090201caed22a48dbc8bfeecc3a382f430
Raymond Guo <rmguo@live.unc.edu>
2020-06-27 20:57:02 -0400

    Added startup screen

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import turtle
+import Tkinter as tk
 
-app = turtle.Screen()
-app.title("Initial window")
-app.setup(width=600, height=600)
+def startGame():
+    game = turtle.Screen()
+    game.title("Initial window")
+    game.setup(width=600, height=600)
+    game.tracer(0)
 
-turtle.mainloop()
+    pen = turtle.Turtle()
+    pen.hideturtle()
+    pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
+
+    turtle.mainloop()
+
+app = tk.Tk(screenName="Snake",baseName="Snake", className="Snake")
+app.title("Snake Game")
+button = tk.Button()
+button["text"] = "Start Game!"
+button["command"] = startGame
+button2 = tk.Button(app, text = "Exit Game", command = quit)
+button.pack()
+button2.pack()
+app.mainloop()
